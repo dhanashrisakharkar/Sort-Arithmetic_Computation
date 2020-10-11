c14ba7d1bd010f1bbfb0d2349a08ce275031eb7e
dhanashri333 <dhanashrisakharkar333@gmail.com>
2020-10-11 18:49:20 +0530

    Added : Store The All Result In Dictionary

--- SortArithmatic.sh
+++ SortArithmatic.sh
@@ -5,8 +5,15 @@ read -p "Enter the First Value  :" a
 read -p "Enter the Secong Value :" b
 read -p "Enter the Third Value  :" c
 
+declare -A Result
+
+Result[1]="$(($a+($b+$c)))"
+Result[2]="$((($a*$b)+$c))"
+Result[3]="$(($a+($b/$c)))"
+Result[4]="$((($a%$b)+$c))"
+
+echo "All result In  Dictionary"  : ${Result[@]}
+echo "All Index  Of  Dictionary"  : ${!Result[@]}
+echo "Total Lenght Of  Dictionary": ${#Result[@]}
 
 
-OUTPUT=$((($a%$b)+$c))
-
-echo  "Output of Arithmetic Computation is" : $OUTPUT
