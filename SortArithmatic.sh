3141c3be8f1c6507992336de0ad5ac3d78fccf64
dhanashri333 <dhanashrisakharkar333@gmail.com>
2020-10-11 15:05:47 +0530

    Added : Take Three Input

--- SortArithmatic.sh
+++ SortArithmatic.sh
@@ -2,3 +2,9 @@
 
 
 echo "Welcome to Sort Arithmetic Computation"
+
+read -p "Enter the First Value  :" a
+read -p "Enter the Secong Value :" b
+read -p "Enter the Third Value  :" c
+
+echo  "All Three Values are" : $a : $b : $c
