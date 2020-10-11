78b39e57e1a5d3f773493f53ac2d29059e35f003
dhanashri333 <dhanashrisakharkar333@gmail.com>
2020-10-11 18:11:02 +0530

    Added : Compute a+b/c

--- SortArithmatic.sh
+++ SortArithmatic.sh
@@ -5,10 +5,8 @@ read -p "Enter the First Value  :" a
 read -p "Enter the Secong Value :" b
 read -p "Enter the Third Value  :" c
 
-PART_TIME_HOUR=8
 
 
-OUTPUT=$(($a*($b+$c)))
+OUTPUT=$(($a+($b/$c)))
 
 echo  "Output of Arithmetic Computation is" : $OUTPUT
-echo  "        Part Time Our Is           " : $PART_TIME_HOUR
